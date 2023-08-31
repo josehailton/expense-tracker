a90aaf731d1cbd395010fbe7dfe667d8afbb0f94
José Hailton <jhsantos3@stefanini.com>
2023-08-31 13:48:25 -0300

    Completed project

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item'
+
+export const items: Item[] = [
+    { date: new Date(2023, 8, 6), category: 'food', title: 'MCDonalds', value: 32.12 },
+    { date: new Date(2023, 8, 15), category: 'food', title: 'Burguer King', value: 28 },
+    { date: new Date(2023, 8, 16), category: 'rent', title: 'Aluguel Apt', value: 2300 },
+    { date: new Date(2023, 9, 18), category: 'salary', title: 'Salário ACME', value: 4500 },
+];
